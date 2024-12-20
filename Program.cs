1f4abd3badd210c2a9d3412f4deebf6d07ebef7f
aleenaabid196 <144983681+aleenaabid196@users.noreply.github.com>
2024-12-20 19:49:04 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ElectoralManagementSystemGUI.BL;
+using ElectoralManagementSystemGUI.DL;
+
+namespace ElectoralManagementSystemGUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Candidate_DL.loadCandidateData("candidate.txt");
+            Citizen_DL.loadCitizenData("citizen.txt");
+            Party_DL.loadPartyData("party.txt");
+            
+
+            Application.Run(new FormSignInSignUp());
+        }
+    }
+}
